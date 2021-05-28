2a1b21f1d1068a29d18d9a5f838154d72b6674f3
angegon <angegon@gmail.com>
2021-05-28 09:37:51 +0200

    0.06

---
+++ MF0491_3/UF1842/Programas/js/js020.js
@@ -0,0 +1,11 @@
+        //Funciones anonimas sin nombre
+        $(document).ready( function() {
+            alert("Hola");
+            $("#ppal").slideUp(2000);
+            $("#ppal").slideDown(3000);
+            $("p").css("background-color", "red");
+            $("p").css("font-size", "20px");
+            $(".prueba").css("color", "blue");
+            $(".prueba").html("Cambiado");
+        });
+        
